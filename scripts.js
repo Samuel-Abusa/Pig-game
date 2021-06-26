37b8c1906b4911344acb493d80dc6752ecefec86
Samuel-Abusa <samuelabusakennedy@gmail.com>
2021-06-26 14:18:01 +0300

    Made a few changes on the markup and the stylesheet then deleted all the javascript

--- scripts.js
+++ scripts.js
@@ -1,12 +1,1 @@
 "use strict";
-
-const rollDice = document.querySelector(".roll");
-const dices = document.querySelector(".dice");
-
-function diceRoll() {
-  let newDice = Math.trunc(Math.random() * 6) + 1;
-  dices.classList.remove("hidden");
-  dices.src = `images/dice-${newDice}.png`;
-}
-
-rollDice.addEventListener("click", diceRoll);
